98ebc2348bb847594617af444021d2ec39bd9455
Olejekglejek <negrutaoleg@gmail.com>
2021-02-04 15:55:12 +0000

    got a job, ill finish it later

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import {useSelector} from 'react-redux'
 import Login from './Views/Login/login'
-import { BrowserRouter as Router} from 'react-router-dom'
+import { BrowserRouter as Router, Switch, Route} from 'react-router-dom'
 import Sidebar from './Views/sidebar/sidebar'
-
-
+import ViewBugPage from './Views/Pages/viewBugs'
+ 
 function App() {
   const {auth} = useSelector(state => state)
   return (
@@ -12,6 +12,10 @@ function App() {
       {!auth.LoggedIn ? <Login /> :
         <>
           <Sidebar />
+          <Switch >
+            <Route path='/viewbugs'><ViewBugPage /></Route>
+          </Switch>
+          
         </>
       }
       </Router>
